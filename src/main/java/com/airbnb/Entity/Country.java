51684938e996681affb112410d77d869e5f3da13
ayush <ayushtiwari.4lz1@gmail.com>
2024-08-09 12:05:42 +0530

    made the changes

--- src/main/java/com/airbnb/Entity/Country.java
+++ src/main/java/com/airbnb/Entity/Country.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name="country")
 @Getter
@@ -16,6 +18,8 @@ public class Country {
     private long id;
     @Column(name="country_name", unique = true,nullable = false,length = 200)
     private String countryName;
+    private String name;
+    private LocalDate date_of_birth;
 
 
 }
